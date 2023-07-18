30da95ac92d8d92df2f63a7db8674cdefeccf3ea
John Nelson <john@abstracttech.com>
2023-07-18 17:09:50 -0400

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by John Nelson on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
