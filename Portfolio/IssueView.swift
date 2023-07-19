205d2a541498ba12b5dc802e0cc36a638438d837
John Nelson <john@abstracttech.com>
2023-07-19 14:23:45 -0400

    add issue list and tag and issue deletion
    Also implement CloudKit updating across devices.

---
+++ Portfolio/IssueView.swift
@@ -0,0 +1,20 @@
+//
+//  IssueView.swift
+//  Portfolio
+//
+//  Created by John Nelson on 7/19/23.
+//
+
+import SwiftUI
+
+struct IssueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IssueView_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueView()
+    }
+}
